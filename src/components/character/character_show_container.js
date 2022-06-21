8f83748dadd4a3469e69b83462e85a56c2156287
Kevin Chen <kevinch28@gmail.com>
2022-06-21 15:35:50 -0400

    remove console logs

--- src/components/character/character_show_container.js
+++ src/components/character/character_show_container.js
@@ -12,7 +12,6 @@ export function withRouter(Children) {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state)
   return {
     character: state.characters,
     location: state.locations,
